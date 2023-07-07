4def046adcd63aa2dcb36c3bb590ba9787c31529
Sardor <henkook-baron@mail.ru>
2023-07-07 23:33:42 +0500

    add removeUser mutation to third task

--- src/tasks/task3.js
+++ src/tasks/task3.js
@@ -214,6 +214,18 @@ const resolvers = {
 
       return users[index];
     },
+    removeUser: (_, args) => {
+      const user = users.find(user => user.id == args.id);
+      const index = users.findIndex(user => user.id == args.id);
+
+      if (!user) {
+        throw new Error(`User not found`);
+      };
+
+      users.splice(index, 1);
+
+      return user;
+    },
     addUserToGroup: (_, args) => {
       group_users.push({
         id: uuidv4(),
